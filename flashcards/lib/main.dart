729085db24d3f3e1a92c1889712be4d53cf87b5e
Lukasz <lukasz.swiatek@icloud.com>
2023-09-26 16:16:03 +0200

    Add firebase

--- flashcards/lib/main.dart
+++ flashcards/lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flashcards/pages/login_page.dart';
 import 'package:flutter/material.dart';
 
+import 'firebase_options.dart';
 import 'pages/home.dart';
 
-void main() {
+void main() async {
+  
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,);
   runApp(const MyApp());
 }
 
@@ -17,7 +23,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: HomePage(),
+      home: LoginPage(),
     );
   }
 }
