e6d5b2a6c2428a0900851416cf8810c8756ecab2
LukaszStudent <sl46672@zut.edu.pl>
2024-09-10 00:36:34 +0200

    Fixed searching for flag

--- flashcards/lib/pages/home_page.dart
+++ flashcards/lib/pages/home_page.dart
@@ -2,7 +2,7 @@ import 'package:country_flags/country_flags.dart';
 import 'package:flashcards/data/flag.dart';
 import 'package:flashcards/pages/details_page.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:hive/hive.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -14,8 +14,27 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   final collectionsName = [];
   final languagesCode = languages.keys.toList(); // Lista kluczy
-  final languagesName = languages.values.toList();
+  late List<String> languagesName;
+  late dynamic filteredLanguages;
   final _collectionController = TextEditingController();
+  final _searchLanguageController = TextEditingController();
+
+  void searchLanguage(String value) {
+    final result = languages.entries.where((entry) =>
+        entry.value.toLowerCase().trim().contains(value.toLowerCase().trim()));
+
+    setState(() {
+      filteredLanguages = result.toList();
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    languagesName = languages.values.toList();
+    filteredLanguages = languages.entries.toList();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,10 +48,31 @@ class _HomePageState extends State<HomePage> {
           : ListView.builder(
               itemCount: collectionsName.length,
               itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text(collectionsName[index]),
-                  onTap: () => Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => DetailsPage())),
+                // final test = languages[index];
+                return Dismissible(
+                  key: Key(index.toString()),
+                  background: const Card(
+                    color: Colors.red,
+                    child: Icon(
+                      Icons.delete,
+                    ),
+                  ),
+                  onDismissed: (direction) {
+                    setState(() {
+                      collectionsName.removeAt(index);
+                    });
+                  },
+                  child: ListTile(
+                    title: Text(collectionsName[index]),
+                    onTap: () => Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => DetailsPage(
+                          collectionName: collectionsName[index],
+                        ),
+                      ),
+                    ),
+                  ),
                 );
               }),
       floatingActionButton: FloatingActionButton(
@@ -40,80 +80,96 @@ class _HomePageState extends State<HomePage> {
           showDialog(
             context: context,
             builder: (context) {
-              return AlertDialog(
-                title: const Text('New flashcards'),
-                content: SizedBox(
-                  width: double.maxFinite, // szerokość Dialogu
-                  height: MediaQuery.sizeOf(context).height / 3,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      TextFormField(
-                        controller: _collectionController,
-                        decoration: InputDecoration(
-                            hintText: "Enter collection name",
-                            border: OutlineInputBorder()),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text("Select language (optional)"),
-                      ),
-                      Expanded(
-                        child: ListView.builder(
-                          itemCount: languagesName.length,
-                          itemBuilder: (context, index) {
-                            return ListTile(
-                              title: Flexible(
-                                child: Row(
+              return StatefulBuilder(builder: (context, setState) {
+                return AlertDialog(
+                  title: const Text('New flashcards'),
+                  content: SizedBox(
+                    width: double.maxFinite, // szerokość Dialogu
+                    height: MediaQuery.sizeOf(context).height / 3,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        TextFormField(
+                          controller: _collectionController,
+                          decoration: const InputDecoration(
+                              hintText: "Enter collection name",
+                              border: OutlineInputBorder()),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 8.0, top: 8.0),
+                          child: TextFormField(
+                            controller: _searchLanguageController,
+                            onChanged: (value) => setState(() {
+                              searchLanguage(value);
+                            }),
+                            decoration: const InputDecoration(
+                                hintText: "Serach language (optional)",
+                                border: OutlineInputBorder()),
+                          ),
+                        ),
+                        Expanded(
+                          flex: 1,
+                          child: ListView.builder(
+                            itemCount: filteredLanguages.length,
+                            itemBuilder: (context, index) {
+                              // print(language);
+                              return ListTile(
+                                title: Row(
                                   children: [
                                     CountryFlag.fromLanguageCode(
-                                        languagesCode[index]),
-
-                                    Text("${languagesName[index]}"),
-                                    // Spacer(),
+                                        filteredLanguages[index].key),
+                                    const SizedBox(
+                                      width: 20,
+                                    ),
+                                    Flexible(
+                                        child: Text(
+                                            filteredLanguages[index].value)),
                                   ],
                                 ),
-                              ),
-                              // onTap: () => setState(
-                              //   () {
-                              //     collectionsName.add(languagesName[index]);
-                              //     // languagesName.removeAt(index);
-                              //     // Navigator.pop(context);
-                              //   },
-                              // ),
-                            );
-                          },
+                                // onTap: () => setState(
+                                //   () {
+                                //     collectionsName.add(languagesName[index]);
+                                //     // languagesName.removeAt(index);
+                                //     // Navigator.pop(context);
+                                //   },
+                                // ),
+                              );
+                            },
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
-                actionsAlignment: MainAxisAlignment.spaceBetween,
-                actions: [
-                  TextButton.icon(
-                    onPressed: () => Navigator.pop(context),
-                    label: Text("Cancel"),
-                    icon: Icon(Icons.cancel_outlined),
-                    style: TextButton.styleFrom(foregroundColor: Colors.red),
-                  ),
-                  TextButton.icon(
-                    onPressed: () {
-                      setState(() {
-                        collectionsName.add(_collectionController.text.trim());
-                      });
-
-                      Navigator.pop(context);
-                    },
-                    label: Text("Add"),
-                    icon: Icon(Icons.add),
-                    style: TextButton.styleFrom(foregroundColor: Colors.green),
-                  )
-                ],
-              );
+                  actionsAlignment: MainAxisAlignment.spaceBetween,
+                  actions: [
+                    TextButton.icon(
+                      onPressed: () => Navigator.pop(context),
+                      label: const Text("Cancel"),
+                      icon: const Icon(Icons.cancel_outlined),
+                      style: TextButton.styleFrom(foregroundColor: Colors.red),
+                    ),
+                    TextButton.icon(
+                      onPressed: () {
+                        setState(() {
+                          collectionsName
+                              .add(_collectionController.text.trim());
+                        });
+                        Hive.openBox(_collectionController.text.trim());
+                        Navigator.pop(context);
+                      },
+                      label: const Text("Add"),
+                      icon: const Icon(Icons.add),
+                      style:
+                          TextButton.styleFrom(foregroundColor: Colors.green),
+                    )
+                  ],
+                );
+              });
             },
           );
         },
-        child: Icon(Icons.language),
+        child: const Icon(Icons.language),
       ),
     );
   }
